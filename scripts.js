b8e1699a3b51583b7a813df63445f8b9119ecee6
asing44 <109570966+asing44@users.noreply.github.com>
2023-04-11 05:12:33 -0400

    4/11/23

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
-var heroMainRightWave = lottie.loadAnimation({
-    container: document.getElementsByClassName("--corner-wave")[0],
+var heroMainRightWave1 = lottie.loadAnimation({
+    container: document.getElementsByClassName("--corner-wave-1")[0],
     renderer: "svg",
     autoplay: true,
     loop: true,
     path: "JSON/heroRightMainWave.json",
 });
+
+var heroMainRightWave2 = lottie.loadAnimation({
+    container: document.getElementsByClassName("--corner-wave-2")[0],
+    renderer: "svg",
+    autoplay: true,
+    loop: true,
+    path: "JSON/heroRightMainWave2.json",
+});
+
+heroMainRightWave1.setSpeed(0.5);
+heroMainRightWave2.setSpeed(0.25);
 
 function mapNumbers(number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
